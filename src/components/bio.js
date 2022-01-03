b0ee7988e3f975ab8955e57d21ed82468ea85867
Srinivas Gunti <srinivasgunti987@gmail.com>
2022-01-03 22:28:57 +0530

    Added blogs

---
+++ src/components/bio.js
@@ -0,0 +1,61 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  const author = data.site.siteMetadata?.author
+  const social = data.site.siteMetadata?.social
+
+  return (
+    <div className="bio">
+      <StaticImage
+        className="bio-avatar"
+        formats={["auto", "webp", "avif"]}
+        src="../../content/assets/profile-pic.jpg"
+        alt="Profile picture"
+      />
+      {author?.name && (
+        <p>
+         {author?.summary || null}
+         <br/>
+          {` `}
+          Follow me on
+          {` `}
+          <a href={`https://linkedIn.com/in/srinivasgunti`}>
+            LinkedIn
+          </a>
+          {`  |  `}
+          <a href={`https://srinivasgunti987.medium.com/`}>
+            Medium
+          </a>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Bio
